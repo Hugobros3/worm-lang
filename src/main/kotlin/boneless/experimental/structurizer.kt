b25931a7377ea5cdfed1473f1fc917f406adcf21
Hugo Devillers <hugo@xol.io>
2021-02-22 13:48:33 +0100

    basics

---
+++ src/main/kotlin/boneless/experimental/structurizer.kt
@@ -0,0 +1,136 @@
+package boneless.experimental
+
+import boneless.util.DotPrinter
+import java.io.Writer
+
+data class Graph(val entryNode: Node)
+
+sealed class Node(val name: String) {
+    val joins = mutableListOf<Branch>()
+    val incommingEdges = mutableListOf<Edge>()
+
+    class Jump(name: String) : Node(name) {
+        lateinit var target: Edge
+    }
+    class Branch(name: String) : Node(name) {
+        lateinit var targets: List<Edge>
+        val rgb = ""
+    }
+}
+
+class Edge(val source: Node, val target: Node) {
+    var signature: List<Node.Branch>? = null
+    var edgeType = EdgeType.UNDEF
+    var isSynthetic: Boolean = false
+}
+
+enum class EdgeType {
+    FORWARD,
+    BACK,
+    UNDEF,
+}
+
+fun visitGraph(graph: Graph, nodeVisitor: (Node) -> Unit, edgeVisitor: (Edge) -> Unit) {
+    val stack = mutableListOf<Node>()
+    val done = mutableSetOf<Node>()
+
+    stack.add(graph.entryNode)
+    while (stack.isNotEmpty()) {
+        val element = stack.removeLast()
+        nodeVisitor(element)
+
+        fun visit(edge: Edge) {
+            edgeVisitor(edge)
+            if (done.contains(edge.target)) {
+
+            } else if (!stack.contains(edge.target)){
+                stack.add(edge.target)
+            }
+        }
+
+        when (element) {
+            is Node.Jump -> visit(element.target)
+            is Node.Branch -> element.targets.forEach(::visit)
+        }
+
+        done.add(element)
+    }
+}
+
+/**
+ * Visits the whole graph depth-first, tags any edges going back to a node in the trace as a back edge.
+ * Hitting a previously hit node that is not along the current path is a forward edge.
+ * This method also populates the incomingEdges variables of each Node
+ */
+fun preprocessEdges(graph: Graph) {
+    val trace = mutableListOf<Node>()
+    val done = mutableSetOf<Node>()
+
+    fun visitNode(element: Node) {
+        trace.add(element)
+
+        fun visit(edge: Edge) {
+            edge.target.incommingEdges.add(edge)
+            if (done.contains(edge.target)) {
+                if (trace.contains(edge.target)) {
+                    edge.edgeType = EdgeType.BACK
+                } else {
+                    edge.edgeType = EdgeType.FORWARD
+                }
+            } else {
+                if (!trace.contains(edge.target)) {
+                    visitNode(edge.target)
+                    edge.edgeType = EdgeType.FORWARD
+                } else {
+                    edge.edgeType = EdgeType.BACK
+                }
+            }
+        }
+
+        when (element) {
+            is Node.Jump -> visit(element.target)
+            is Node.Branch -> element.targets.forEach(::visit)
+        }
+
+        done.add(element)
+        trace.removeLast()
+    }
+    visitNode(graph.entryNode)
+}
+
+fun createDomtree() {
+    // http://www.cs.rice.edu/~keith/EMBED/dom.pdf
+}
+
+class CFGGraphPrinter(output: Writer) : DotPrinter(output) {
+
+    init {
+        output += "digraph MethodCFG {"
+        indent++
+        output += "bgcolor=transparent;"
+    }
+
+    fun print(graph: Graph, nodeColor: String) {
+        val nodeAppearance = NodeAppearance(fillColor = nodeColor, style = "filled")
+        val ControlFlow = DotPrinter.ArrowStyle(arrowHead = "normal")
+
+        fun nodeVisitor(node: Node) {
+            this.node(node.name, if (node is Node.Branch) "X" else "", nodeAppearance)
+        }
+        fun edgeVisitor(edge: Edge) {
+            val label = when(edge.edgeType) {
+                EdgeType.FORWARD -> ""
+                EdgeType.BACK -> "back"
+                EdgeType.UNDEF -> "undef"
+            }
+            this.arrow(edge.source.name, edge.target.name, ControlFlow, label)
+        }
+        visitGraph(graph, ::nodeVisitor, ::edgeVisitor)
+    }
+
+    fun finish() {
+
+        indent--
+        output += "}"
+    }
+}
---
+++ src/test/kotlin/boneless/experimental/TestStructurizer.kt
@@ -0,0 +1,54 @@
+package boneless.experimental
+
+import org.junit.Test
+import java.io.File
+import java.util.*
+
+class TestStructurizer {
+    fun make_random_cfg(): Graph {
+        var i = 0
+        fun name(): String = "${i++}"
+
+        val rnd = Random()
+        val nodesCount = 16; // 4 + rnd.nextInt(8)
+        val nodes = (0..nodesCount).map {
+            val branchWeight = 0.35
+            if (rnd.nextFloat() < branchWeight) {
+                Node.Branch(name())
+            } else {
+                Node.Jump(name())
+            }
+        }
+
+        for (node in nodes) {
+            when(node) {
+                is Node.Branch -> {
+                    node.targets = listOf(Edge(node, nodes.random()), Edge(node, nodes.random()))
+                }
+                is Node.Jump -> {
+                    node.target = Edge(node, nodes.random())
+                }
+            }
+        }
+
+        return Graph(nodes.random())
+    }
+
+    @Test
+    fun test() {
+        val dotFile = File("test_out/structurizer/graph.dot")
+        dotFile.parentFile.mkdirs()
+        val w = dotFile.writer()
+        val graph = make_random_cfg()
+
+        preprocessEdges(graph)
+
+        val p = CFGGraphPrinter( w)
+
+        p.print(graph, "aquamarine")
+
+        p.finish()
+
+        w.close()
+    }
+}
